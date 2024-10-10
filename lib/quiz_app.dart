4cfbd89ccdd1df7c42403de47db577582d9734a4
bijuaryal17@gmail.com <bijuaryal17@gmail.com>
2024-10-10 20:12:11 +0545

    Setting folders structure

---
+++ lib/quiz_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QuizApp extends StatefulWidget {
+  const QuizApp({super.key});
+
+  @override
+  State<QuizApp> createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("Hello World")),
+    );
+  }
+}
